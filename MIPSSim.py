8c813895fc1a47d13068f4fd3fde1ae6b90f81d7
hanz94 <hansikam94@gmail.com>
2021-10-04 18:59:38 -0400

    implemented writing disassembled code to memory

--- MIPSSim.py
+++ MIPSSim.py
@@ -1,7 +1,9 @@
 STARTING_MEMORY_ADDRESS = 260
 memory = []
-decoded_memory = []
+disassembled_memory = []
 registerFile = []
+data_memory_pointer = 0
+
 
 def readFileLineByLine(filename):
     file1 = open(filename, 'r')
@@ -11,6 +13,13 @@ def readFileLineByLine(filename):
         itemList.append(line.strip())
     file1.close()
     return itemList
+
+
+def write_deassembled_code_to_file(memory, decoded_memory):
+    open('dout.txt', 'w').close()
+    with open('dout.txt', 'a') as the_file:
+        for word in memory:
+            the_file.write(memory + "\n")
 
 
 memory = readFileLineByLine("sample.txt")
